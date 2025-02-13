2b31f2913b3af11844add648bb0c4bb128bb9e8e
Eshan Chathuranga Fernando <eshanchathurangafernando@gmail.com>
2025-02-14 03:07:17 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/test/'
+  base: '/test/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
